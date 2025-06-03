79881444abfc875c0c40de0c22fbbcd8c6086de0
JohnSwiftC <johnswift@live.com>
2025-06-03 12:56:57 -0500

    add player, position structs and impls

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,41 @@ use minifb::{Window, WindowOptions};
 const WINDOW_W: usize = 1000;
 const WINDOW_H: usize = 1000;
 const FPS: usize = 30;
+const FOCAL_DISTANCE: u32 = 1;
+
+struct Player {
+    position: Position,
+    view_angle: i32, // Principal axis is facing down, deviation is in radians.
+}
+
+impl Player {
+    /// Updates absolute position
+    fn set_position(&mut self, x: i32, y: i32) {
+        self.position.x = x;
+        self.position.y = y;
+    }
+
+    /// Updates relative position
+    fn update_position(&mut self, x: i32, y: i32) {
+        self.position.x += x;
+        self.position.y += y;
+    }
+
+    /// Updates absolute angle
+    fn set_angle(&mut self, theta: i32) {
+        self.view_angle = theta;
+    }
+
+    /// Updates relative angle
+    fn update_angle(&mut self, theta: i32) {
+        self.view_angle += theta;
+    }
+}
+
+struct Position {
+    x: i32,
+    y: i32, // this is a 2d x,y coordinate plane
+}
 
 fn main() {
     let mut window = Window::new(
@@ -12,18 +47,24 @@ fn main() {
         WindowOptions::default()
     ).expect("Window failed to open.");
 
-    //window.set_target_fps(FPS);
-
     let mut red = from_u8_rgb(255, 50, 120);
     let blue = from_u8_rgb(0, 0, 255);
     let mut red_buffer: Vec<u32> = vec![red; WINDOW_H * WINDOW_W];
 
-    window.set_target_fps(30);
+    window.set_target_fps(FPS);
+
+    let map = [
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
 
     // Keep window open
     loop {
         window.update_with_buffer(&red_buffer, WINDOW_W, WINDOW_H).expect("Failed to update window buffer");
-        red = decrease_brightness(red, 1);
+        red = decrease_brightness(red, 5);
         red_buffer = vec![red; WINDOW_H * WINDOW_W];
     }
 }
